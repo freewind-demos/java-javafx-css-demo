b656a765674eb5379db6f836b9ae85afdc0b5b1a
freewind <nowindlee@gmail.com>
2018-07-16 15:46:11 +0800

    css demo

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-JavaFX Hello World Demo
-=======================
+JavaFX Css Demo
+===============
 
-Show a window with `Hello, JavaFX` label.
+We have two ways:
 
+1. `Node.setStyle("-fx-background-color: GREEN")` with inline styles
+2. `Node.getStylesheets().add("hello.css")` to load css files, which has css classes defined
+
+We can't define css classes in Java code directly, they have to be in some real file.
--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,8 +1,10 @@
 package demo;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,9 +17,35 @@ public class Hello extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello");
         VBox root = new VBox() {{
-            getChildren().add(new Label("Hello, JavaFX!"));
+            getChildren().add(fromCssFile());
+            getChildren().add(defineStyleDirectly());
+            getChildren().add(defineCssClassesInString());
         }};
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
+    }
+
+    private HBox fromCssFile() {
+        return new HBox() {{
+            setPadding(new Insets(10));
+            this.getStylesheets().add("hello.css");
+            this.getStyleClass().add("my-rect");
+            getChildren().add(new Label("From Css File"));
+        }};
+    }
+
+    private HBox defineStyleDirectly() {
+        return new HBox() {{
+            setPadding(new Insets(10));
+            setStyle("-fx-background-color: GREEN");
+            getChildren().add(new Label("Define style in code"));
+        }};
+    }
+
+    private HBox defineCssClassesInString() {
+        return new HBox() {{
+            setPadding(new Insets(10));
+            getChildren().add(new Label("Try define css classes just in Code, \nbut is not possible. \nThe css classes have to be defined in a real file. \nWe can use a temporary file if we need to generate some css classes dynamically"));
+        }};
     }
 }
